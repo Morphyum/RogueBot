615b65b8e88b49d5d7fe4ba493acca43fe0bd840
Morphyum <zion@c-schwabe.de>
2018-07-30 09:55:01 +0200

    *moep

--- RogueBotExe/RogueBotExe/Program.cs
+++ RogueBotExe/RogueBotExe/Program.cs
@@ -23,6 +23,22 @@ namespace RogueBot {
 
         private async Task MessageReceived(SocketMessage message) {
             if (message.Channel.Id == 455410123926405121) {
+                if (message.Content == "!bot") {
+                    await message.Channel.SendMessageAsync(
+                        "!logs - Log locations\n" +
+                        "!map - Link to the ongoing mapping progress\n" +
+                        "!pins - Let people know about the pins\n" +
+                        "!rt - Link to the mod download\n" +
+                        "!rtfm - Link to the RogueTech manual\n" +
+                        "!wartech - Link to the WarTech manual\n"
+                        );
+                }
+                if (message.Content == "!pins") {
+                    await message.Channel.SendMessageAsync("Please check the pins located at the top right (the little pin needle symbol).");
+                }
+                if (message.Content == "!map") {
+                    await message.Channel.SendMessageAsync("https://docs.google.com/spreadsheets/d/1FXDZMTEZwp71qCxqnaW3MpaB1ZfovaJudvZrM6pnOGk/edit?usp=sharing");
+                }
                 if (message.Content == "!ping") {
                     await message.Channel.SendMessageAsync("Pong!");
                 }
@@ -35,10 +51,13 @@ namespace RogueBot {
                 if (message.Content == "!wartech") {
                     await message.Channel.SendMessageAsync("https://www.nexusmods.com/battletech/articles/76");
                 }
+                if (message.Content == "!logs") {
+                    await message.Channel.SendMessageAsync("BATTLETECH\\Mods\\BTModLoader.log\nBATTLETECH\\Mods\\\\.modtek\\ModTek.log\nBATTLETECH\\BattleTech_Data\\output_log.txt\n+ log from relevant mod folder if possible (example: BATTLETECH\\Mods\\MODNAME\\log.txt)\n\nJust drag and drop them in here.");
+                }
                 if (message.Content == "!pone") {
                     await message.Channel.SendMessageAsync("<@248490263457038336>");
 
-                    await message.Channel.SendMessageAsync("▒▒▒▒░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓\n▒▒▒░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓\n▒▒░▐▌░▒░░░░░░▒▒▒▒▒▒▒▒▒\n▒▒░░▌░░░░░░░░░░▒▒▒▒▒▒▒\n▒▒▒▒░░░░░░░░░░░░▓▓▓▒▒▒\n▒▒▒▒▒▒░░▀▀███░░░░▓▒▒▒▓\n▒▒▒▒▒▒░▌▄████▌░░░▓▒▒▒▓\n▒▒▒▒▒░░███▄█▌░░░▓▓▒▓▓▓\n▒▒▒▒▒▒▒░▀▀▀▀░░░░▓▓▒▒▓▓\n▒▒▒▒▒▒▒▒░░░░░░░░░▓▓▓▓▓\n▒▒▒▒▒▒▒░░░░░░▐▄░▓▓▓▓▓▓\n▒▒▒▒▒▒░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+                    await message.Channel.SendMessageAsync("\n▒▒▒▒░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓\n▒▒▒░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓\n▒▒░▐▌░▒░░░░░░▒▒▒▒▒▒▒▒▒\n▒▒░░▌░░░░░░░░░░▒▒▒▒▒▒▒\n▒▒▒▒░░░░░░░░░░░░▓▓▓▒▒▒\n▒▒▒▒▒▒░░▀▀███░░░░▓▒▒▒▓\n▒▒▒▒▒▒░▌▄████▌░░░▓▒▒▒▓\n▒▒▒▒▒░░███▄█▌░░░▓▓▒▓▓▓\n▒▒▒▒▒▒▒░▀▀▀▀░░░░▓▓▒▒▓▓\n▒▒▒▒▒▒▒▒░░░░░░░░░▓▓▓▓▓\n▒▒▒▒▒▒▒░░░░░░▐▄░▓▓▓▓▓▓\n▒▒▒▒▒▒░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
                 }
             }
         }
